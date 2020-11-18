b373f61984a788076853eb0a3d2dae9673ac3d6a
T3CH <jlp02.dk@gmail.com>
2020-11-18 20:51:31 +0100

    Working Bot

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import discord
 from discord.ext import commands
+from secrets import *
+
+client = commands.Bot(command_prefix=PREFIX)
+
+@client.event
+async def on_ready():
+    print(f"Bot is running as {client.user}")
+
+client.run(TOKEN)
