15b9b33bb4d838ec0db77582649e8a6d32583de2
T3CH <jlp02.dk@gmail.com>
2020-11-18 21:03:51 +0100

    Added Ping command and command handler

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import discord
 from discord.ext import commands
 from secrets import *
+import os
 
 client = commands.Bot(command_prefix=PREFIX)
 
-@client.event
-async def on_ready():
-    print(f"Bot is running as {client.user}")
+commands = discord
+
+for file in os.listdir("./Commands"):
+    if file.endswith(".py"):
+        print(f"Loaded : {file[:-3]}")
+        client.load_extension(f"Commands.{file[:-3]}")
+
+client.load_extension("Events")
 
 client.run(TOKEN)
