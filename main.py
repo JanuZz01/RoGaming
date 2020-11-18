80f1e6db7777e980eecdc8554e58621d37e97b37
T3CH <jlp02.dk@gmail.com>
2020-11-18 21:12:58 +0100

    Updated Command Handler and added comments

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ from discord.ext import commands
 from secrets import *
 import os
 
-client = commands.Bot(command_prefix=PREFIX)
+client = commands.Bot(command_prefix=c_settings.PREFIX)
 
 commands = discord
 
 for file in os.listdir("./Commands"):
     if file.endswith(".py"):
-        print(f"Loaded : {file[:-3]}")
+        print(f"Loaded : {file[:-3]} command(s)/event(s)")
         client.load_extension(f"Commands.{file[:-3]}")
 
-client.load_extension("Events")
-
-client.run(TOKEN)
+client.run(c_settings.TOKEN)
